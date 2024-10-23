8303d460fc78ed161a5344a8ae08bbc70e46af9e
Ikunctrl007 <yzg136202203@163.com>
2024-10-23 10:53:05 +0800

    初始化内容

---
+++ src/main/java/com/nnxx/FlyglobalApplication.java
@@ -0,0 +1,13 @@
+package com.nnxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyglobalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyglobalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nnxx/FlyglobalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnxx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyglobalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
