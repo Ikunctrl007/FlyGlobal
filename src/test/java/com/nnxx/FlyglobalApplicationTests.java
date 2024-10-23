14be66cb84e60b91fec507b7d05d65469c3bac03
Ikunctrl007 <yzg136202203@163.com>
2024-10-23 23:21:06 +0800

    忽略Test文件

--- src/test/java/com/nnxx/FlyglobalApplicationTests.java
+++ src/test/java/com/nnxx/FlyglobalApplicationTests.java
@@ -1,13 +1,19 @@
 package com.nnxx;
 
+import com.nnxx.util.JwtUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class FlyglobalApplicationTests {
 
     @Test
     void contextLoads() {
+//        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+//        String encode = bCryptPasswordEncoder.encode("123456");
+//        System.out.println(encode);
+        System.out.println(JwtUtils.parseJWT("eyJhbGciOiJIUzI1NiJ9.eyJUb2tlbiI6MSwiZXhwIjoxNzM0MDEyMjAyfQ.EgkjiQGr_yTxp4idkONJxkVy1BwSo2cwFqjlqOxkEt4"));
     }
 
 }
