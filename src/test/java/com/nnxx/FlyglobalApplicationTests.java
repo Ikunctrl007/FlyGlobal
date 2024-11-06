c25f56ffafe55a0634164adab1aececfd0a42c30
Ikunctrl007 <yzg136202203@163.com>
2024-11-06 09:59:11 +0800

    哈哈哈

--- src/test/java/com/nnxx/FlyglobalApplicationTests.java
+++ src/test/java/com/nnxx/FlyglobalApplicationTests.java
@@ -10,10 +10,10 @@ class FlyglobalApplicationTests {
 
     @Test
     void contextLoads() {
-//        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 //        String encode = bCryptPasswordEncoder.encode("123456");
-//        System.out.println(encode);
-        System.out.println(JwtUtils.parseJWT("eyJhbGciOiJIUzI1NiJ9.eyJUb2tlbiI6MSwiZXhwIjoxNzM0MDEyMjAyfQ.EgkjiQGr_yTxp4idkONJxkVy1BwSo2cwFqjlqOxkEt4"));
+        System.out.println(bCryptPasswordEncoder.matches("","$2a$10$yl0Z34nNwlYGr5jvDtaQXuraftop41bgAlUKDcE/wnNQVZ6PXC0.y"));
+//        System.out.println(JwtUtils.parseJWT("eyJhbGciOiJIUzI1NiJ9.eyJUb2tlbiI6MSwiZXhwIjoxNzM0MDEyMjAyfQ.EgkjiQGr_yTxp4idkONJxkVy1BwSo2cwFqjlqOxkEt4"));
     }
 
 }
